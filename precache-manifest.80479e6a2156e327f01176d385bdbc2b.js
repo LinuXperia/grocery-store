5ec59e29a6aa48007e70bd78667445150938638c
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-05-21 20:30:32 -0400

    Updates

---
+++ precache-manifest.80479e6a2156e327f01176d385bdbc2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c421e4854bffae1c95d44e6b6fd034b2",
+    "url": "/Amitabh-K.github.io/grocery-store/index.html"
+  },
+  {
+    "revision": "09fbae1aed15d72aa5ba",
+    "url": "/Amitabh-K.github.io/grocery-store/static/css/main.1878b47c.chunk.css"
+  },
+  {
+    "revision": "416fda67d41ca424367f",
+    "url": "/Amitabh-K.github.io/grocery-store/static/js/2.2f1f2a43.chunk.js"
+  },
+  {
+    "revision": "09fbae1aed15d72aa5ba",
+    "url": "/Amitabh-K.github.io/grocery-store/static/js/main.958a6510.chunk.js"
+  },
+  {
+    "revision": "d4dcb1a2e6fc110e1048",
+    "url": "/Amitabh-K.github.io/grocery-store/static/js/runtime~main.b04b1c45.js"
+  },
+  {
+    "revision": "2121bb971be169f4aa920bfd9387c89e",
+    "url": "/Amitabh-K.github.io/grocery-store/static/media/logo1.2121bb97.JPG"
+  }
+]);
